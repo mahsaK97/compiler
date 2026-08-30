3c2fb9cc51f9b2fec0536c9b984d2a018c0a7971
Suha <mahsa.keshtpur.1005@gmail.com>
2026-08-30 14:26:52 +0430

    Organize compiler project structure

---
+++ Scr/lexer.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+
+
+
+enum class Token_type
+{
+
+   LEFT_PAREN,
+   RIGHT_PAREN,
+
+   LEFT_BRACE,
+   RIGHT_BRACE,
+
+   LEFT_BRACKET,
+   RIGHT_BRACKET,
+
+   COMMA,
+   SEMICOLON,
+
+   PLUS,
+   MINUS,
+   STAR,
+   SLASH,
+
+   BANG,
+   EQUAL,
+
+   LESS,
+   GREATER,
+
+   BABG_EQUAL,
+   EQUAL_EQUAL,
+
+   LESS_EQUAL,
+   GREATER_EQUAL,
+
+   AND_AND,
+   OR_OR,
+
+   IDETIFIRE;
+   STRING,
+   NUMBER,
+
+   LET,
+   STRING_TYPE,
+   BOOL,
+   FUNCTION,
+   RETURN,
+   PRINT,
+   IF,
+   ELSE,
+   WHILE,
+   TRUE,
+   FALSE,
+
+   END_OF_FILE
+
+};
